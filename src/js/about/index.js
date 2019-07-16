add427328a6f67b28c4f308e5d34e3b9ea518ee4
DESKTOP-NGQC278\jack <330447171@qq.com>
2019-07-16 21:59:48 +0800

    可以使用tween来做动画，顺便研究下tween的源码

--- src/js/about/index.js
+++ src/js/about/index.js
@@ -1,5 +1,24 @@
 import '@/css/base.css';
 import '@/css/about/index.css';
 
-import './tw.min.js'
+import TWEEN from '@tweenjs/tween.js'
 
+
+var box = document.querySelector('.zmz-info');
+
+function animate(time) {
+    requestAnimationFrame(animate);
+    TWEEN.update(time);
+}
+requestAnimationFrame(animate);
+
+
+var coords = { x: 0, y: 0 };
+var tween = new TWEEN.Tween(coords)
+        .to({ x: 0, y: 30 }, 1000)
+        .onUpdate(function() { 
+            box.style.setProperty('transform', 'translate(' + coords.x + 'px, '     + coords.y + 'px)');
+        })
+        .start();
+
+// end
