c4c095592ccbee70821ac68703198fd5836d1d15
DESKTOP-NGQC278\jack <330447171@qq.com>
2019-07-20 22:10:35 +0800

    动画优化

--- src/js/about/index.js
+++ src/js/about/index.js
@@ -1,10 +1,11 @@
 import '@/css/base.css';
 import '@/css/about/index.css';
 
+// 哒哒哒
 import TWEEN from '@tweenjs/tween.js'
 
 
-var box = document.querySelector('.zmz-info');
+var boxList = document.querySelectorAll('.zmz-info >div');
 
 function animate(time) {
     requestAnimationFrame(animate);
@@ -12,13 +13,17 @@ function animate(time) {
 }
 requestAnimationFrame(animate);
 
-
-var coords = { x: 0, y: 0 };
-var tween = new TWEEN.Tween(coords)
-        .to({ x: 0, y: 30 }, 1000)
-        .onUpdate(function() { 
-            box.style.setProperty('transform', 'translate(' + coords.x + 'px, '     + coords.y + 'px)');
-        })
-        .start();
+boxList.forEach((val, idx) => {
+    setTimeout(() => {
+        var coords = { x: 0, y: 0, o: 0};
+        new TWEEN.Tween(coords)
+            .to({ x: 8, y: 0, o: 1 }, 666)
+            .onUpdate(function() { 
+                val.style.setProperty('transform', 'translate(' + coords.x + 'px, ' + coords.y + 'px)');
+                val.style.setProperty('opacity', coords.o);
+            })
+            .start();
+    }, idx * 1000);
+})
 
 // end
